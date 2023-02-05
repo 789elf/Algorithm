1874c1d8c1279b48d44e8013a62ccc4a29a61849
789elf <789elf@naver.com>
2023-02-05 22:03:10 +0900

    lv0 03

--- src/programmers/Lv0_02_Divide.java
+++ src/programmers/Lv0_02_Divide.java
@@ -1,9 +1,13 @@
 package programmers;
 
 public class Lv0_02_Divide {
+    public static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
 
 	}
 
---
+++ src/programmers/Lv0_03_CheckNumbers.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class Lv0_03_CheckNumbers {
+    public static int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+     }
+    
+    public static void main(String args[]) {
+    	System.out.println(solution(1, 1));
+    }
+
+}
